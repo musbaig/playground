376402d20fd878fc0635901d31d95a7e1f90d4d1
Muslim Baig <muslimbaig@gmail.com>
2014-04-28 01:44:32 -0400

    A better trie. Stronger, faster, sexier.

--- scripts/trie.js
+++ scripts/trie.js
@@ -1,20 +1,51 @@
 'use strict';
 
-function generateTrie(words) {
-  var trie = {};
-  words.forEach(function(word) {
-    var letters = word.split(""),
-        current = trie;
-    for(var j = 0; j < letters.length; j++) {
-      var letter = letters[j], position = current[letter];
-      if(position == null) {
-        current = current[letter] = j === letters.length - 1 ? {$: 1} : {};
-      } else if(position === 1) {
-        current = current[letter] = {$: 1};
-      } else {
-        current = current[letter];
-      }
+//function generateTrie(words) {
+//  var trie = {};
+//  words.forEach(function(word) {
+//    var letters = word.split(""),
+//        current = trie;
+//    for(var j = 0; j < letters.length; j++) {
+//      var letter = letters[j], position = current[letter];
+//      if(position == null) {
+//        current = current[letter] = j === letters.length - 1 ? {$: 1} : {};
+//      } else if(position === 1) {
+//        current = current[letter] = {$: 1};
+//      } else {
+//        current = current[letter];
+//      }
+//    }
+//  });
+//  return trie;
+//}
+
+var Trie = function() {
+  this.root = {};
+};
+
+Trie.prototype.validate = function(word) {
+  if((word === undefined) || (word === null) || (typeof word != "string")) {
+    throw "Word exception error!"
+  }
+};
+
+Trie.prototype.insert = function(word) {
+  var trie = this.root;
+  trie = word.split("").reduce(function(curTrie, letter, indx, arr) {
+    if(!(letter in curTrie)) {
+      curTrie[letter] = {};
     }
-  });
-  return trie;
-}
+    return curTrie[letter];
+  }, trie);
+
+  trie.$ = 1;
+};
+
+Trie.prototype.multi_insert = function(words) {
+  words.map(function(word, indx, arr) {
+    this.insert(word);
+  }, this);
+};
+
+Trie.prototype.hasWord = function(word) {
+};
