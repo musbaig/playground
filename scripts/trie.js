01aa0f6ffedbe28cf33f417e36745d3a6798ee7c
Muslim Baig <muslimbaig@gmail.com>
2014-09-08 00:41:49 -0400

    First pass at a more complete trie implementation.

--- scripts/trie.js
+++ scripts/trie.js
@@ -24,37 +24,84 @@ var Trie = function() {
 };
 
 Trie.prototype.validate = function(word) {
-  if((word === undefined) || (word === null) || (typeof word != "string")) {
-    throw "Word exception error!"
+  if((word === undefined) || (word === null)) {
+    throw "Given word is invalid!"
+  }
+  if (typeof word != "string") {
+    throw "Given word is not a string!";
   }
 };
 
 Trie.prototype.insert = function(word) {
+  this.validate(word);
+
   var trie = this.root;
-  trie = word.split("").reduce(function(curTrie, letter, indx, arr) {
-    if(!(letter in curTrie)) {
-      curTrie[letter] = {};
+  trie = word.split("").reduce(function(currentTrie, letter) {
+    if (!(letter in currentTrie)) {
+      currentTrie[letter] = {};
     }
-    return curTrie[letter];
+    return currentTrie[letter]; // return obj associated with that letter
   }, trie);
 
+//  trie.$ = (trie.$ || 0) + 1; // TODO potentially counts num of inserts of same word
   trie.$ = 1;
 };
 
 Trie.prototype.multi_insert = function(words) {
   var T = this;
-  words.map(function(word, indx, arr) {
+  words.map(function(word) {
     T.insert(word);
   }, T);
 };
 
 Trie.prototype.hasWord = function(word) {
+  this.validate(word);
+
+  var trie = this.root;
+  word.split("").some(function(letter) {
+    if (letter in trie) {
+      trie = trie[letter];
+      return false;
+    } else {
+      trie = {};
+      return true;
+    }
+  });
+
+//  return trie.$ > 0; // TODO address generic trie
+  return trie.$ === 1;
 };
 
 Trie.prototype.autocomplete = function(prefix) {
-  if(!prefix && prefix.length == 0) {
+  if (!prefix && prefix.length == 0) {
     return [];
   }
   var trie = this.root,
       prefix_arr = prefix.split("");
+
+  for(var i = 0; i < prefix_arr.length; i++) {
+    var letter = prefix_arr[i];
+    if(letter in trie) {
+      trie = trie[letter];
+    } else {
+      return [];
+    }
+  }
+
+  function trieWords(curTrie, word, words) {
+    for(var letter in curTrie) {
+      if(letter == "$") {
+        words.push(word);
+      } else {
+        trieWords(curTrie[letter], word + letter, words);
+      }
+    }
+
+    return words;
+  }
+  var words = trieWords(trie, "", []);
+
+  return words.map(function(elt) {
+    return prefix + elt;
+  })
 };
