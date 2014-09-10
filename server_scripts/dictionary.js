f30b5907e40caa0a1300e4568b58ad935263cc7e
Muslim Baig <muslimbaig@gmail.com>
2014-09-10 15:48:30 -0400

    Fix Hapi dictionary service intializer

--- server_scripts/dictionary.js
+++ server_scripts/dictionary.js
@@ -6,7 +6,7 @@ exports.init = function() {
   fs.readFile('./resources/words.txt', {encoding: 'utf8', flag: 'r'},
       function(err, data) {
         if (err) throw err;
-        var words = data.split('\r\n');
+        var words = data.split('\n');
         Trie.multi_insert(words.map(function(elt) {
           return elt.toLowerCase();
         }));
